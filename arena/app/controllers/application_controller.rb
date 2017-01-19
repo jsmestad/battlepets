da96579c066c23781dfe50c7bb2ea223ae5079d0
Justin Smestad <justin.smestad@gmail.com>
2017-01-19 16:02:58 -0700

    Port config files from Manager

--- arena/app/controllers/application_controller.rb
+++ arena/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::UnpermittedParameters do |e|
+    errors = params.collect do |param|
+      {title: 'Param not allowed', detail: "#{param} is not allowed."}
+    end
+    render json: {errors: errors}, status: :bad_request
+  end
+
+  rescue_from ActionController::ParameterMissing do |e|
+    errors = params.collect do |param|
+      {title: 'Missing Parameter', detail: "The required parameter, #{param}, is missing."}
+    end
+    render json: {errors: errors}, status: :bad_request
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    head :not_found
+  end
 end
+
